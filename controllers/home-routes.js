f654e0b69881add86a894085aacc13fcb6a2d508
Ian Sills <iansills04@gmail.com>
2023-11-05 22:13:47 -0600

    my handle bar is not showing data all the way

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,17 +1,35 @@
 const router = require('express').Router();
-const { Gallery, Painting } = require('../models');
+const { Post, User, Comments } = require('../models');
 
-router.get('/', async (req, res) => {
-    try {
+// router.get('/', async (req, res) => {
+//     try {
         
-        res.render('homepage', {
-        galleries,
-        loggedIn: req.session.loggedIn,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+//         res.render('post');
+//         console.log()
+//     } catch (err) {
+//       console.log(err);
+//       res.status(500).json(err);
+//     }
+//   });
 
-  module.exports = router;
+ 
+
+  router.get('/', async (req, res) => {
+    try {
+        const dbPostsData = await Post.findAll();
+        // console.log("dbPostData: ", dbPostsData);
+        const postsData = dbPostsData.map((post)=> {
+            // console.log("inside map: with post: ", post);
+            return post.get({ plain: true});
+        });
+
+        console.log("postData: ", postsData);
+        res.render('homepage', {postsData});
+
+    } catch (err) {
+        console.log(err);
+        res.status.json(err);
+    }
+}); 
+
+module.exports = router;
